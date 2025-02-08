633ccb6ba51243781bca436289a76d6ce8cf91c4
curiosity29 <dung.dm195860@sis.hust.edu.vn>
2025-02-08 07:22:10 +0700

    just fix some broken abstract script

--- Script/Component/CharacterManager.gd
+++ Script/Component/CharacterManager.gd
@@ -3,10 +3,11 @@ extends Node
 
 
 func update(event: Event):
-    var summirized_events = Summarizer.summarize_events(event)
-    for character in event.characters:
-        character.memory.append(summirized_events)
-    
-
-    for relationship in relationships:
-        relationship.update()
+	#var summirized_events = Summarizer.summarize_events(event)
+	#for character in event.characters:
+		#character.memory.append(summirized_events)
+	#
+#
+	#for relationship in relationships:
+		#relationship.update()
+		pass
--- Script/Component/RelationshipManager.gd
+++ Script/Component/RelationshipManager.gd
@@ -3,21 +3,21 @@ extends Node
 
 
 func update_relationships(event: Event) -> void:
-    
-    for character_pair in get_pair_array_from_array(event.characters):
-        var char1 = character_pair[0]
-        var char2 = character_pair[1]
-        var current_relationship = char1.relationships[char2] if char2 in char1.relationships else char1.relationships[char2] = Relationship.new()
-        
-        # ...
+	
+	for character_pair in get_pair_array_from_array(event.characters):
+		var char1 = character_pair[0]
+		var char2 = character_pair[1]
+		#var current_relationship = char1.relationships[char2] if char2 in char1.relationships else char1.relationships[char2] = Relationship.new()
+		
+		# ...
 
-func update_relationships(event, current_relationship):
-    pass
+func update_pair_relationship(event, current_relationship):
+	pass
 
 func get_pair_array_from_array(array: Array) -> Array:
-    var pairs = []
-    for i in range(array.size()):
-        for j in range(i+1, array.size()):
-            if i != j:
-                pairs.append([array[i], array[j]])
-    return pairs
+	var pairs = []
+	for i in range(array.size()):
+		for j in range(i+1, array.size()):
+			if i != j:
+				pairs.append([array[i], array[j]])
+	return pairs
